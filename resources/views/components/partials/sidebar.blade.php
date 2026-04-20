c3e4a00c9423dd5de9571ef56173ae8700818123
DerandaBagas17 <111202314906@mhs.dinus.ac.id>
2026-04-20 13:57:42 +0700

    Tugas Pertemuan 4

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -74,7 +74,8 @@
                 class="{{ $baseLink }} {{ request()->routeIs('obat.*') ? $active : $inactive }}">
                 <i class="fas fa-pills w-4 text-center"></i>
                 Manajemen obat
-            </a>            
+            </a>
+       
 
         </div>
         @endif
@@ -94,6 +95,11 @@
                 <i class="fas fa-house-medical w-4 text-center"></i>
                 Dashboard Pasien
             </a>
+            <a href="{{ route('pasien.daftar') }}"
+   class="{{ $baseLink }} {{ request()->routeIs('pasien.daftar') ? $active : $inactive }}">
+    <i class="fas fa-house-medical w-4 text-center"></i>
+    Pendaftaran Periksa
+</a>
 
 
         </div>
@@ -113,6 +119,11 @@
                 class="{{ $baseLink }} {{ request()->routeIs('dokter.dashboard') ? $active : $inactive }}">
                 <i class="fas fa-stethoscope w-4 text-center"></i>
                 Dashboard Dokter
+            </a>
+                 <a href="{{ route('jadwal-periksa.index') }}"
+                class="{{ $baseLink }} {{ request()->routeIs('jadwal-periksa.*') ? $active : $inactive }}">
+                <i class="fas fa-stethoscope w-4 text-center"></i>
+                Jadwal Periksa
             </a>
 
         </div>
